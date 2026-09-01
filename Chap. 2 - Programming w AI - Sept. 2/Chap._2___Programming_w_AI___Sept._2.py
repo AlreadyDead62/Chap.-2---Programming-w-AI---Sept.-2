2456be8dfbad38ae3f81b4763e4179ded53b90b4
jeric <jeric@JJ-LAPTOP>
2026-09-01 15:38:08 -0400

    Add project files.

---
+++ Chap. 2 - Programming w AI - Sept. 2/Chap._2___Programming_w_AI___Sept._2.py
@@ -0,0 +1,22 @@
+"""  
+This program adds  20, 10, 30.
+"""
+a = 20
+b = 10
+c = 30
+print(a + b + c)
+
+"""
+OR  I could write it like this.....
+ nums = [20, 10, 30]
+ print(sum(nums))
+
+ which will give me 60 as the output.
+ """
+
+""" 
+This program will display 30,20,10,40 and 50. 
+And then it will display the max of the 5 numbers.
+"""
+
+print(max(30,20,10,40,50))
